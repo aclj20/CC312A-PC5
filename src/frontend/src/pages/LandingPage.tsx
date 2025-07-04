a1122c721dcb57f93de190af4f6661d8353665f1
amirmiir <00aamircg@gmail.com>
2025-07-04 11:54:38 -0500

    chore: frontend setup

--- src/frontend/src/pages/LandingPage.tsx
+++ src/frontend/src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingPage: React.FC = () => {
     return (
-        <div>
+        <div className="font-bold text-4xl">
             Landing Page
         </div>
     )
