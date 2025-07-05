978c4371933740d433cd193cafdf028340efd0e9
amirmiir <00aamircg@gmail.com>
2025-07-05 08:30:41 -0500

    chore: delegated router to App.tsx

--- src/frontend_service/src/App.tsx
+++ src/frontend_service/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import { RouterProvider } from 'react-router';
+import router from './routers/router';
+import './App.css';
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
