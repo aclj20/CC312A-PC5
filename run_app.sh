22c23f83ad6463d2a74faaed26a41f838d34a1df
amirmiir <00aamircg@gmail.com>
2025-07-05 11:02:18 -0500

    feat: update run_app.sh for services up

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e
 
 ROOT_PROJECT_DIR=$(dirname "$0")
 
+# cambiamos al directorio src
+cd "$ROOT_PROJECT_DIR/src"
+docker compose -f docker-compose.base.yaml -f docker-compose.dev.yaml up -d
+
+echo "http://localhost:8002" 
